c1374c61a5dad9b30aa84832ace69ae33be6ff16
kathrynspeltz <kathrynspeltz@gmail.com>
2023-08-06 18:22:58 -0500

    first upload

---
+++ write_code.js
@@ -0,0 +1,72 @@
+function addToZero(arr) {
+    let answer = false
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                answer = true
+            }
+        }
+    } return answer
+}
+//console.log(addToZero([]));
+// -> False
+//.log(addToZero([1]));
+// -> False
+//console.log(addToZero([1, 2, 3]));
+// -> False
+//console.log(addToZero([1, 2, 3, -2]));
+// -> True
+
+//AddtoZero would have a runtime of O(n^2)
+
+
+function hasUniqueChars(str) {
+    let answer = true
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            if ((i != j) && (str[i] === str[j])) {
+                answer = false
+            }
+        }
+    } return answer
+}
+
+// console.log(hasUniqueChars("Monday"));
+// -> True
+// console.log(hasUniqueChars("Moonday"));
+// -> False
+// console.log(hasUniqueChars("Momday"));
+// -> False
+
+function isPangram(str) {
+    let answer = 0
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            if ((i != j) && (str[i] === str[j]) && ((/[a-zA-Z]/).test(str[j]) === true)) {
+                answer += 1
+            }
+        }
+    } if (answer === 26) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+// -> True
+// console.log(isPangram("I like cats, but not mice"));
+// -> False)
+
+function findLongestWord(arr) {
+    let longestLength = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > longestLength) {
+            longestLength = arr[i].length
+        }
+    } return longestLength
+}
+console.log(findLongestWord(["hi", "hello", "hey"]));
+// -> 5)
